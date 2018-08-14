db744f6c52edd9e6b87dd3e2cbf86d36d80ab65f
samliweisen <liweisen007@gmail.com>
2018-08-14 13:52:00 +0000

    refactor decode_data

--- visuals/views.py
+++ visuals/views.py
@@ -96,9 +96,9 @@ def get_imdb_id(request):
     douban_url = 'https://movie.douban.com/subject/' + douban_id
     url_content = urllib3.PoolManager().request('GET', douban_url)
     decode_data = url_content.data.decode('utf-8')
-    imdb_list = re.findall('href="http://www.imdb.com/title/(.*?)"', url_content.data.decode('utf-8'))
+    imdb_list = re.findall('href="http://www.imdb.com/title/(.*?)"', decode_data)
     # get list of release dates from webpage
-    release_dates = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}\([\u4e00-\u9fff]+\)', url_content.data.decode('utf-8'))
+    release_dates = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}\([\u4e00-\u9fff]+\)', decode_data)
     # remove duplicate release date
     release_dates = list(set(release_dates))
     imdb_id = ''
