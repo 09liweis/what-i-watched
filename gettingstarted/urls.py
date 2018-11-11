b1a0172f4150001c3b68c97cb5eec05a89f63c5b
samliweisen <liweisen007@gmail.com>
2018-11-11 21:11:33 +0000

    update index url

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,8 +11,8 @@ import visuals.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    # url(r'^$', visuals.views.index, name='index'),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', visuals.views.index, name='index'),
+    # url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
     
     
