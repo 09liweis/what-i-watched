fa6cc79ae5a72c28ce5ca286d3019a6bf97deb13
samliweisen <liweisen007@gmail.com>
2018-11-18 02:10:18 +0000

    remove statiefiles_dirs

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -135,11 +135,11 @@ ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
